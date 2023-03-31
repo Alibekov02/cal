3b833332dbd5affaf6f65518528b23e4b197164e
Alibekov02 <126797669+Alibekov02@users.noreply.github.com>
2023-03-31 11:29:58 +0600

    working version calculator

---
+++ MyCalculator/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows;
+using MyCalculator.Command;
+using MyCalculator.ViewModels.Base;
+using static System.Net.Mime.MediaTypeNames;
+using System.Data;
+
+namespace MyCalculator.ViewModels
+{
+    internal class CalculatorViewModel:ViewModelBase
+	{
+		#region Заголовок окна
+		private string _display = "0";
+		public string Display
+		{
+			get => _display;
+			set => Set(ref _display, value);
+		}
+		#endregion
+		
+		public ICommand ClickButton { get;  }
+		private bool CanClickButtonExecute(object parameter) => true;
+		private void OnClickButtonExecited(object parameter)
+		{
+			string paramterText = parameter.ToString();
+			
+			try
+			{
+				
+				string list = "123456789";
+				if (paramterText == "C")
+				{
+					Display = "";
+					
+				}
+
+				else if (paramterText == "=")
+				{
+					Display = new DataTable().Compute(Display, null).ToString();
+				}
+				else if (paramterText == "X")
+				{
+					Display = Display.Remove(Display.Length - 1, 1);
+				}
+				else if (Display == "0")
+				{
+
+					if (paramterText == "0") { Display = "0"; }
+					if (list.IndexOf(paramterText.ToString()) != -1) { Display = paramterText.ToString(); }
+
+				}
+
+				else Display += paramterText.ToString();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+		public CalculatorViewModel()
+		{
+
+			ClickButton = new LambdaCommand( OnClickButtonExecited, CanClickButtonExecute);
+		}
+	}
+}
+
